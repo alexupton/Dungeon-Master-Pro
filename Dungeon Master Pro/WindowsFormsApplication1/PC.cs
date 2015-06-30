8a037261702df74c7c4dc69a62033cdf2c25f248
alexupton <alexander.upton@ndsu.edu>
2015-06-29 19:59:30 -0500

    PC class
    Started work on PC class

---
+++ Dungeon Master Pro/WindowsFormsApplication1/PC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class PC
+    {
+        string name { get; set; }
+        string player { get; set; }
+        CharClass cClass { get; set; }
+        Race race { get; set; }
+        string alignment { get; set; }
+        string diety { get; set; }
+        string size { get; set; }
+        int age { get; set; }
+        char gender { get; set; }
+        int height { get; set; }
+        int weight { get; set; }
+        string eyeColor { get; set; }
+
+    }
+}
