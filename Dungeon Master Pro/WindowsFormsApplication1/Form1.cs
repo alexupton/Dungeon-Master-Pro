724431cac9657ac430af3c377baa3884fe622c12
alexupton <alexander.upton@ndsu.edu>
2015-06-30 00:54:23 -0500

    Revert "I dunno"
    This reverts commit 74d18b47f8f26fe4c47145a03c0e50f9d4abe988.

--- Dungeon Master Pro/WindowsFormsApplication1/Form1.cs
+++ Dungeon Master Pro/WindowsFormsApplication1/Form1.cs
@@ -29,6 +29,7 @@ namespace WindowsFormsApplication1
         private void button1_Click_2(object sender, EventArgs e)
         {
             CharCreateInstructions instruct = new CharCreateInstructions(this);
+            this.Enabled = false;
             instruct.Show();
         }
     }
